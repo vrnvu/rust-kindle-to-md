d3b857ddd3d3e5c4b2d6b7ec21455d23e0233d99
ad <hi@arnaudiaz.com>
2024-08-22 18:51:55 +0200

    iter read lines and hash set

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,17 @@ use collection::{Collection, Quote};
 
 pub mod collection;
 
-fn read_lines(filename: &str) -> Vec<String> {
-    let mut result = Vec::new();
-
-    for line in read_to_string(filename).unwrap().lines() {
-        result.push(line.to_string())
-    }
-
-    result
+fn read_lines(filename: &str) -> anyhow::Result<Vec<String>> {
+    Ok(read_to_string(filename)?
+        .lines()
+        .map(|l| l.to_string())
+        .collect())
 }
 
 fn read_hashes_from_file(file_path: &str) -> anyhow::Result<HashSet<String>> {
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
-
-    let mut hashes = HashSet::new();
-    for hash in reader.lines() {
-        let hash = hash?;
-        hashes.insert(hash);
-    }
-
+    let hashes: HashSet<String> = reader.lines().collect::<Result<HashSet<_>, _>>()?;
     Ok(hashes)
 }
 
@@ -47,7 +38,7 @@ fn main() -> anyhow::Result<()> {
     };
 
     let file_clippings = "My Clippings.txt";
-    let lines = read_lines(file_clippings);
+    let lines = read_lines(file_clippings)?;
 
     let file_filters = "filters.txt";
     let filters = read_hashes_from_file(file_filters)?;
